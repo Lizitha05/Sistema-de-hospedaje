f0f5a01360ee18fd1959f4e51f4f70db4abf4f56
Silver25Life <112659585+Silver25Life@users.noreply.github.com>
2025-06-01 14:51:57 -0600

    Nueva Carpeta sqlBeto
    Aqui estan mis sqls

---
+++ sqlBeto/SP_Usuarios.sql
@@ -0,0 +1,100 @@
+﻿--ESTE QUERY ES ESPECIFICAMENTE PARA PROCEDURES DE LOS USUARIOS 🗣🗣🗣
+--INICIAR SESION
+--Declaramos el procedure
+CREATE PROCEDURE SP_ValidaUser 
+--Variables que va a recibir el procedure, en este caso:
+--Correo
+--Contraseña
+@SP_Correo VARCHAR(100),
+@SP_Contraseña VARCHAR(100)
+AS
+BEGIN
+
+--DML usamos el select para seleccionar la tabla
+SELECT nombres, correo, contra --Seleciona la
+FROM --Tabla de
+Usuario --NombreTabla
+WHERE  correo = @SP_Correo AND contra = @SP_Contraseña ;--Donde 
+
+END
+
+
+
+--GESTION DE USUARIOS , select, insert, update , delete
+
+CREATE PROCEDURE SP_GestionDeUsuario
+    @Opc CHAR(1),
+    @SP_idUsuario INT = NULL,
+    @SP_tipoUsuario VARCHAR(50) = NULL,
+    @SP_correo VARCHAR(100) = NULL,
+    @SP_contra VARCHAR(100) = NULL,
+    @SP_nombres VARCHAR(50) = NULL,
+    @SP_apellidoPaterno VARCHAR(50) = NULL,
+    @SP_apellidoMaterno VARCHAR(50) = NULL,
+    @SP_numeroNomina VARCHAR(100) = NULL,
+    @SP_fechaNacimiento DATE = NULL,
+    @SP_telefono1 VARCHAR(10) = NULL,
+    @SP_telefono2 VARCHAR(10) = NULL,
+    @SP_usuarioRegistro VARCHAR(50) = NULL,
+    @SP_fechaRegistro DATE = NULL,
+    @SP_horaRegistro TIME = NULL
+
+AS 
+BEGIN 
+
+IF @Opc = 'S'  --Si la opcion es igual a select 
+BEGIN -- Comienza
+
+SELECT * --Selecciona la
+FROM Usuario  --Tabla de
+WHERE Activo = 1;  --Donde Activo sea igual a 1
+
+END --fin del select
+
+ELSE IF @Opc='I' --Entonces si la opcion es insert
+BEGIN -- Comienza
+
+INSERT INTO Usuario  --Inserta en la tabla de
+(tipoUsuario,correo ,contra ,nombres,apellidoPaterno ,apellidoMaterno ,numeroNomina ,
+fechaNacimiento ,telefono1 ,telefono2 ,usuarioRegistro ,fechaRegistro ,horaRegistro )
+VALUES  -- los siguientes valores
+(@SP_tipoUsuario,@SP_correo ,@SP_contra ,@SP_nombres ,@SP_apellidoPaterno ,@SP_apellidoMaterno ,@SP_numeroNomina
+,@SP_fechaNacimiento ,@SP_telefono1 ,@SP_telefono2 , @SP_usuarioRegistro ,@SP_fechaRegistro ,@SP_horaRegistro)
+
+END  --fin del insert
+
+ELSE IF @Opc = 'U' --Entonces si la opcion es Update
+BEGIN --Comienza
+
+UPDATE Usuario  --Actualiza la tabla de Usuario
+SET  -- los siguientes parametros
+
+tipoUsuario = @SP_tipoUsuario,
+correo = @SP_correo,
+contra = @SP_contra ,
+nombres = @SP_nombres,
+apellidoPaterno = @SP_apellidoPaterno,
+apellidoMaterno= @SP_apellidoMaterno ,
+numeroNomina = @SP_numeroNomina,
+fechaNacimiento= @SP_fechaNacimiento ,
+telefono1 = @SP_telefono1,
+telefono2= @SP_telefono2,
+usuarioRegistro = @SP_usuarioRegistro,
+fechaRegistro = @SP_fechaRegistro,
+horaRegistro = @SP_horaRegistro
+
+WHERE  idUsuario = @SP_idUsuario   --valida por medio de la llave
+
+END  --Fin del update
+
+
+ELSE IF @Opc = 'D' --Entonces si es igual a delete
+BEGIN --Comienza
+
+UPDATE Usuario  --Actualiza usuario 
+SET Activo = 0  --En la variable
+WHERE idUsuario = @SP_idUsuario  --Donde la llave sea igua a id usuario
+
+END --Finaliza
+
+END
---
+++ sqlBeto/tablasv2.sql
@@ -0,0 +1,132 @@
+--Hecho por alberto
+-- Crear base de datos y usarla
+--CREATE DATABASE BD_Hotels;
+GO
+USE BD_Hotels;
+GO
+
+-- Tabla Usuario
+CREATE TABLE Usuario (
+    idUsuario INT IDENTITY(1,1) PRIMARY KEY,
+    tipoUsuario VARCHAR(50) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    contra VARCHAR(100) NOT NULL,
+    nombres VARCHAR(50) NOT NULL,
+    apellidoPaterno VARCHAR(50) NOT NULL,
+    apellidoMaterno VARCHAR(50) NOT NULL,
+    numeroNomina VARCHAR(100) NOT NULL,
+    fechaNacimiento DATE NOT NULL,
+    telefono1 VARCHAR(10) NOT NULL,
+    telefono2 VARCHAR(10) NULL,
+    usuarioRegistro VARCHAR(50) NOT NULL,
+    fechaRegistro DATE NOT NULL,
+    horaRegistro TIME NOT NULL,
+    Activo BIT NOT NULL DEFAULT 1
+);
+
+-- Tabla Hotel
+CREATE TABLE Hotel (
+    idHotel INT IDENTITY(1,1) PRIMARY KEY,
+    nombreHotel VARCHAR(100) NOT NULL,
+    ubicacion VARCHAR(100) NOT NULL,
+    domicilio VARCHAR(100) NOT NULL,
+    numeroPisos VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    fechaIniOpe VARCHAR(100) NOT NULL,
+    usuarioRegistro VARCHAR(100) NOT NULL,
+    fechaRegistro DATE NOT NULL,
+    horaRegistro TIME NOT NULL
+);
+
+-- Tabla Cliente
+CREATE TABLE Cliente (
+    idCliente INT IDENTITY(1,1) PRIMARY KEY,
+    nombreCompleto VARCHAR(100) NOT NULL,
+    apellidoMaterno VARCHAR(100) NOT NULL,
+    apellidoPaterno VARCHAR(100) NOT NULL,
+    ubicacion VARCHAR(100) NOT NULL,
+    RFC VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    telefono1 VARCHAR(10) NOT NULL,
+    telefono2 VARCHAR(10) NULL,
+    fechaNacimiento DATE NOT NULL,
+    estadoCivil VARCHAR(20),
+    usuarioCliente VARCHAR(50),
+    fechaCliente DATE NOT NULL,
+    horaRegistro TIME NOT NULL
+);
+
+-- Tabla TipoDeHabitacion
+CREATE TABLE TipoDeHabitacion (
+    idTipoHabitacion INT IDENTITY(1,1) PRIMARY KEY,
+    numeroCamas VARCHAR(100) NOT NULL,
+    tipoCamas VARCHAR(100) NOT NULL,
+    precioNochePersona VARCHAR(100) NOT NULL,
+    cantidadHabitacion VARCHAR(100) NOT NULL,
+    nivelHabitacion VARCHAR(100) NOT NULL,
+    caracteristicas VARCHAR(100) NOT NULL,
+    amenidades VARCHAR(10) NOT NULL,
+    cantidadPersonas INT NOT NULL,
+    usuarioRegistro VARCHAR(50) NOT NULL,
+    fechaRegistro DATE NOT NULL,
+    horaRegistro TIME NOT NULL
+);
+
+-- Tabla Habitacion
+CREATE TABLE Habitacion (
+    idHabitacion INT IDENTITY(100,1) PRIMARY KEY,
+    estadoReservacion VARCHAR(50) NOT NULL,
+    idTipoHabitacion INT,
+    FOREIGN KEY (idTipoHabitacion) REFERENCES TipoDeHabitacion(idTipoHabitacion)
+);
+
+-- Tabla Reserva
+CREATE TABLE Reserva (
+    folioFactura INT IDENTITY(100,10) PRIMARY KEY,
+    costoHospedaje DECIMAL(10,2) NOT NULL,
+    numeroDeNoches INT NOT NULL,
+    serviciosAdicionales VARCHAR(100) NOT NULL,
+    descuentosAplicados DECIMAL(10,2) NOT NULL,
+    codigoReservacion UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL,
+    estadoReservacion VARCHAR(50) NOT NULL,
+    CheckIn BIT NULL,
+    CheckOut BIT NULL,
+    fechaReservacion DATE NULL,
+    fechaCancelacion DATE NULL,
+    anticipo DECIMAL(10,2) NULL,
+    usuarioRegistro VARCHAR(50) NOT NULL,
+    montoTotal VARCHAR(20),
+    idCliente INT,
+    idHotel INT,
+    FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente),
+    FOREIGN KEY (idHotel) REFERENCES Hotel(idHotel)
+);
+
+-- Tabla ReservaDetalle
+CREATE TABLE ReservaDetalle (
+    idReservaDetalles INT IDENTITY(1,1) PRIMARY KEY,
+    numeroHabitaciones INT NOT NULL,
+    numeroHuespedesTotal INT NOT NULL,
+    idTipoHabitacion INT,
+    folioFactura INT,
+    FOREIGN KEY (idTipoHabitacion) REFERENCES TipoDeHabitacion(idTipoHabitacion),
+    FOREIGN KEY (folioFactura) REFERENCES Reserva(folioFactura)
+);
+
+-- Tabla RegistroHotel
+CREATE TABLE RegistroHotel (
+    idRegistroHotel INT IDENTITY(1,1) PRIMARY KEY,
+    idUsuario INT,
+    idHotel INT,
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
+    FOREIGN KEY (idHotel) REFERENCES Hotel(idHotel)
+);
+
+-- Tabla RegistroCliente
+CREATE TABLE RegistroCliente (
+    idRegistroCliente INT IDENTITY(1,1) PRIMARY KEY,
+    idUsuario INT,
+    idCliente INT,
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
+    FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente)
+);
